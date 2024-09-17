c707f7421d037128c5e245795c714f5fa551c7a3
unknown <RedlE22004@sus.htl-grieskirchen.at>
2024-09-17 15:32:26 +0200

    Hausübung

---
+++ FirstHourPOS/src/EratosthenesPrimeSieve.java
@@ -0,0 +1,35 @@
+public class EratosthenesPrimeSieve implements PrimeSieve{
+    private int obergrenze;
+    public EratosthenesPrimeSieve(int obergrenze){
+        this.obergrenze = obergrenze;
+    }
+
+
+    @Override
+    public boolean isPrime(int p) {
+        if (p < 2) {
+            return false;
+        }
+
+        boolean[] gestrichen = new boolean[obergrenze + 1];
+
+        for (int i = 2; i <= Math.sqrt(obergrenze); i++) {
+            if (!gestrichen[i]) {
+                for (int j = i * i; j <= obergrenze; j += i) {
+                    gestrichen[j] = true;
+                }
+            }
+        }
+
+        return !gestrichen[p];
+    }
+
+    @Override
+    public void printPrimes() {
+        for (int i = 0; i < obergrenze; i++){
+            if (isPrime(i)){
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ FirstHourPOS/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+    EratosthenesPrimeSieve eratosthenesPrimeSieve = new EratosthenesPrimeSieve(100);
+    eratosthenesPrimeSieve.printPrimes();
+    }
+}
